e61ff851799559ba1e1510a37e5daad2ecbe3d9e
Roy Shpiner <shpiner11@Roys-MacBook-Air.local>
2024-02-02 19:36:31 +0200

    first

--- SpellChecker.java
+++ SpellChecker.java
@@ -3,7 +3,9 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
+		//System.out.println(levenshtein("hell0", "hello"));
 		String word = args[0];
+
 		int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
@@ -11,25 +13,89 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		return str.substring(1);
+
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
-	}
+		int levboth;
+		int leva;
+		int levb;
+
+		if (word1.length()==0){
+			return word2.length();
+		}
+		if (word2.length() == 0){
+			return word1.length();
+		}
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		leva = levenshtein(tail(word1), word2);
+		levb = levenshtein(word1, tail(word2));
+		levboth = levenshtein(tail(word1), tail(word2));
+
+		
+
+		if(word1.charAt(0)==word2.charAt(0)){
+			return levboth;
+		} 
+		else{
+			if (leva <= levb){
+				if (leva <= levboth){
+					return 1+leva;
+				}
+				else{
+					return 1+levboth;
+				}
+
+			}
+			else if (levb <= levboth){
+				return 1+levb;
+			}
+			else{
+				return 1+levboth;
+			}	
+			}
+			
+		}
+		
+
+	
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
+		String word ="";
 		In in = new In(fileName);
+		for (int i = 0; i<dictionary.length;i++){
+			word = in.readString();
+			dictionary[i] = word;
+		}
 
-		// Your code here
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		int min = levenshtein(word, dictionary[0]);
+		String closeString = dictionary[0];
+		int current = min;
+		for(int i=1;i<dictionary.length;i++){
+			current = levenshtein(word, dictionary[i]);
+			if (current<=min){
+				min = current;
+				closeString = dictionary[i];
+				System.out.println(closeString);
+			}
+
+		}
+		if (min <= threshold){
+			return closeString;
+		}
+		else{
+			return word;
+		}
+
 	}
 
 }
